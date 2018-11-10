8a48b1aa6195713f0252c19dbb46191f295776c9
liammcdonald97 <liammcdonald97@gmail.com>
2018-11-09 21:42:21 -0500

    Add project files.

---
+++ structs_137/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace structs_137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 2.4m;
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
